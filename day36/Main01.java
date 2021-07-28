2430e8baecc99dd7e00deebe33928e5bf1e388f0
pryor-pan <80051868+pryor-pan@users.noreply.github.com>
2021-07-28 12:23:41 +0800

    Add files via upload

---
+++ day36/Main01.java
@@ -0,0 +1,34 @@
+package dayoneQs_3.day36;
+
+import java.util.Scanner;
+
+/**
+ * @Description:
+ * @Author: panlai
+ * @Date: 2021/7/24 14:38
+ */
+
+public class Main01 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        while (sc.hasNext()) {
+            String str1 = sc.next();
+            String str2 = sc.next();
+            String[] strings1 = str1.split("/");
+            String[] strings2 = str2.split("/");
+            int a1 = Integer.parseInt(strings1[0]);
+            int a2 = Integer.parseInt(strings1[1]);
+            int b1 = Integer.parseInt(strings2[0]);
+            int b2 = Integer.parseInt(strings2[1]);
+            int af = 0;
+            int al = 0;
+            if (a1>a2){
+
+            }
+
+
+
+        }
+    }
+
+}
---
+++ day36/Main02.java
@@ -0,0 +1,11 @@
+package dayoneQs_3.day36;
+
+/**
+ * @Description:
+ * @Author: panlai
+ * @Date: 2021/7/24 14:39
+ */
+
+public class Main02 {
+
+}
