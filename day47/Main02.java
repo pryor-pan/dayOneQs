0861114c443a990a3d686bb56efa5f3b2ef0fefe
pryor-pan <80051868+pryor-pan@users.noreply.github.com>
2021-08-10 12:26:59 +0800

    Add files via upload

---
+++ day47/Main02.java
@@ -0,0 +1,16 @@
+package dayoneQs_3.day47;
+
+/**
+ * @Name: Main02
+ * @Description:        马戏团
+ * @Author: panlai
+ * @Date: 2021/8/6 11:40
+ */
+
+public class Main02 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
